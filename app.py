e1cfc8c90f1f42d57354cee48c5cb1a167ab5321
Mohammed Ragab Saad Attia <mohammedr25102005@gmail.com>
2023-03-07 23:05:39 +0200

    edit app

--- app.py
+++ app.py
@@ -14,25 +14,3 @@ model = load_model(str(THIS_FOLDER / "models/model.h5"))
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
-    features = ['BMXWT', 'BMXHT', 'BMXBMI', 'RIDAGEYR', 'MGXH1T1', 'MGXH2T1', 'MGXH1T2', 'MGXH2T2', 'MGXH1T3', 'MGXH2T3', 'MGDCGSZ', 'RIAGENDR']
-    x = pd.DataFrame([int_features], columns=features);
-
-    numiric_cols = ['BMXWT', 'BMXHT', 'BMXBMI', 'RIDAGEYR', 'MGXH1T1', 'MGXH2T1', 'MGXH1T2', 'MGXH2T2', 'MGXH1T3', 'MGXH2T3', 'MGDCGSZ']
-    scaler = preprocessing.StandardScaler().fit(x[numiric_cols])
-    x_scaled = scaler.transform(x[numiric_cols])
-    x_scaled = pd.DataFrame(x_scaled, index=x.index, columns=x[numiric_cols].columns)
-    x_scaled = pd.concat([x_scaled, x['RIAGENDR']], axis=1)
-    x_scaled
-
-    prediction = model.predict(x)
-    output = prediction[0]
-    print(output)
-
-    return render_template('index.html', prediction_text='Percent with osteoporosis is {}'.format(output))
-
-#if __name__ == '__main__':
-#   app.run(debug = True)
