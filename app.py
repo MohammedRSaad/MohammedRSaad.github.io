50c4c87d794cb9fcc93d8dfb0e82bdafbcce49e8
Mohammed Ragab Saad Attia <mohammedr25102005@gmail.com>
2023-03-07 20:33:32 +0200

    adding the updated app.py

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ import pandas as pd
 from sklearn import preprocessing
 from flask import Flask, request, render_template
 from keras.models import load_model
+from pathlib import Path
+
+THIS_FOLDER = Path(__file__).parent.resolve()
+print(THIS_FOLDER)
 
 app = Flask(__name__)
-model = load_model('models/model.h5')
+model = load_model(str(THIS_FOLDER / "models/model.h5"))
 
 @app.route('/')
 def home():
@@ -16,7 +20,7 @@ def predict():
     int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
     features = ['BMXWT', 'BMXHT', 'BMXBMI', 'RIDAGEYR', 'MGXH1T1', 'MGXH2T1', 'MGXH1T2', 'MGXH2T2', 'MGXH1T3', 'MGXH2T3', 'MGDCGSZ', 'RIAGENDR']
     x = pd.DataFrame([int_features], columns=features);
-    
+
     numiric_cols = ['BMXWT', 'BMXHT', 'BMXBMI', 'RIDAGEYR', 'MGXH1T1', 'MGXH2T1', 'MGXH1T2', 'MGXH2T2', 'MGXH1T3', 'MGXH2T3', 'MGDCGSZ']
     scaler = preprocessing.StandardScaler().fit(x[numiric_cols])
     x_scaled = scaler.transform(x[numiric_cols])
@@ -26,6 +30,7 @@ def predict():
 
     prediction = model.predict(x)
     output = prediction[0]
+    print(output)
 
     return render_template('index.html', prediction_text='Percent with osteoporosis is {}'.format(output))
 
