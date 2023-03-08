5490fddc8e2a18e6c4bbba4468690d938e2270e9
Mohammed Ragab Saad <103288444+MohammedRSaad@users.noreply.github.com>
2023-03-09 01:32:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def predict():
 
     return render_template('index.html', prediction_text='Percent with osteoporosis is {}'.format(output))
 
-if __name__ == '__main__':
-   app.run(debug = True)
+#if __name__ == '__main__':
+#   app.run(debug = True)
